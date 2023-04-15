17528b8a1cf960ddf9a128d0c1080dc69a47f53d
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2023-04-15 20:27:28 +0300

    last

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import appearGoblin from "./js/appearGoblin.js";
 import clickCell from "./js/clickCell.js";
+import "./css/style.css";
 
 const cells = Array.from(document.querySelectorAll(".cell")); // находим все ячейки игрового поля
 
